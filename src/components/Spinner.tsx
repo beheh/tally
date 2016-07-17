d082d3981532f2242eac5ed7550f519cd91fcdbf
Benedict Etzel <developer@beheh.de>
2016-07-17 17:00:41 +0200

    Replace Font Awesome arrows with CC0 cog
    Credit to @davidmerfield (http://publicicons.org/).

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -6,6 +6,6 @@ interface SpinnerProps extends React.ClassAttributes<any> {
 
 export default class Spinner extends React.Component<SpinnerProps, any> {
 	render():JSX.Element {
-		return <span><i className="fa fa-refresh fa-spin fa-fw"></i> {this.props.message}&hellip;</span>;
+		return <div className="spinner"><img src="img/gear.svg" /> <span>{this.props.message}&hellip;</span></div>;
 	}
 }
